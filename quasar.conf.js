5745ef1555ac375773be3622e196fe4aeadbc66c
Sam Smith <ss.samsmith98@outlook.com>
2020-12-09 19:53:21 +0000

    change config

--- quasar.conf.js
+++ quasar.conf.js
@@ -44,7 +44,6 @@ module.exports = function (ctx) {
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       vueRouterMode: 'hash', // available values: 'hash', 'history'
-      publicPath: ctx.dev ? '/' : '/PopCatSite/',
       // transpile: false,
 
       // Add dependencies for transpiling with Babel (Array of string/regex)
